76b829f3b4fea9a4b399d129a7f85dfa2c45a6e8
1102137242 <ww0986360075@gmail.com>
2016-03-03 15:03:25 +0800

    test

---
+++ 1102137242/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitDemoForKUAS
+{
+    class hello
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("GOGOGOGOGOGO!!");
+            System.Console.ReadLine();
+        }
+    }
+}
